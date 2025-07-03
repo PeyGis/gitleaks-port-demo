43c187f2281631e5a4cf7deac5433f8db39f69f9
Pages Coffie <isaac.p.coffie@gmail.com>
2025-07-03 18:29:39 +0000

    update secrets

--- oops.ts
+++ oops.ts
@@ -1,4 +1,4 @@
 // some fake secrets to mess around with
 
 const discord_client_secret = '8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ'
-const FastlyAPIToken = "uhZtofOcNzoH8F5-m0bzsLvCqIjzNFG"
+const FastlyAPIToken = "uhZtofOcNzoH895-m0bzsLvCqIjzNFG"
